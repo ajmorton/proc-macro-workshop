f191b1f005d13e00315c3751ab0cb0dc8aebc818
Damien Stanton <damien.stanton@gmail.com>
2019-09-11 16:49:02 -0400

    passing up to test 5, following along with stream for 6 to understand some of this typing logic

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,14 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-
 use syn::{
     parse_macro_input,
     DeriveInput,
     Ident,
+    Data::Struct,
+    DataStruct,
+    Fields::Named,
+    FieldsNamed,
+    Type::Path
 };
 use quote::quote;
 
@@ -15,48 +19,115 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let ident = &tree.ident;
     let ident_builder = Ident::new(&format!("{}Builder", ident), ident.span());
 
-    // starting code gen
-    // -----------------
+    // untangling this gnarly path plagiarized directly from jonhoo's stream on this workshop
+    let fields = if let Struct(DataStruct{
+        fields: Named(FieldsNamed{
+            ref named,
+            ..
+        }),
+        ..
+    }) = tree.data {
+        named
+    } else { unimplemented!() }; // TODO: Think about how to enable enums and unions
+    
+    let is_optional = |t: &syn::Type| {
+        if let Path(ref t_path) = t {
+            return t_path.path.segments.len() == 1 
+                    && t_path.path.segments[0].ident == "Option";
+        }
+        false
+    };
+    
+    // TODO: pick up here, ~ 1:32:37 in stream
+    // let unwrap_t = |t: &syn::Type| -> syn::Type {
+    //     if let Path(ref t_path) = t {
+    //        assert!(t_path.path.segments.len() == 1 && t_path.path.segments[0].ident == "Option");
+    //     }
+    //     unreachable!();
+    // };
+
+    let opt_fields = fields.iter().map(|f| {
+        let inner_name = &f.ident;
+        let inner_type = &f.ty;
+        if is_optional(&f.ty) {
+            quote! {
+                #inner_name: #inner_type
+            }
+        } else {
+            quote! {
+                #inner_name: std::option::Option<#inner_type>
+            }
+        }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let inner_name = &f.ident;
+        let inner_type = &f.ty;
+        if is_optional(&f.ty) {
+            quote! {
+                pub fn #inner_name(&mut self, #inner_name: #inner_type) -> &mut Self {
+                    self.#inner_name = #inner_name;
+                    self
+                }
+            }
+        
+        } else {
+            quote! {
+                pub fn #inner_name(&mut self, #inner_name: #inner_type) -> &mut Self {
+                    self.#inner_name = Some(#inner_name);
+                    self
+                }
+            }
+        }
+    });
+    
+    let nil_fields= fields.iter().map(|f| {
+        let inner_name = &f.ident;
+        quote! {
+            #inner_name: None
+        }
+    });
+
+
+    let build_fields = fields.iter().map(|f| {
+        let inner_name = &f.ident;
+        if is_optional(&f.ty) {
+            quote! {
+                #inner_name: self.#inner_name.clone()
+            }
+        } else {
+            quote! {
+                #inner_name: self.#inner_name.clone().ok_or(concat!(stringify!(#inner_name), " cannot be None"))?
+            }
+        }
+    });
+
     let quoted = quote! {
+        use std::error::Error;
+
         pub struct #ident_builder {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#opt_fields,)*
         }
 
         impl #ident {
             pub fn builder() -> #ident_builder {
                  #ident_builder {
-                     executable: None,
-                     args: None,
-                     env: None,
-                     current_dir: None,
+                    #(#nil_fields,)*
                  }
             }
         }
 
         impl #ident_builder {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
+            pub fn build(&mut self) -> Result<#ident, Box<dyn Error>> {
+                Ok(#ident{
+                    #(#build_fields,)*
+                })
             }
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args= Some(args);
-                self
-            }
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env= Some(env);
-                self
-            }
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir= Some(current_dir);
-                self
-            }
+            
+            #(#methods)*
+
         }
     };
-    // ---------------
-    // ending code gen
 
     TokenStream::from(quoted)
 }
