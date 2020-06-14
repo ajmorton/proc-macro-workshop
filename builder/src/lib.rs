cb56e453dd7abd53c44da157bab9066efd96abea
ajmorton <ajmorton2@gmail.com>
2020-06-14 14:31:56 +1000

    Restructure - quote! individual fields, not as list
    Create BuilderField struct to store important field info.
    quote! individual BuilderField items, don't batch quote! list of fields

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -13,67 +13,25 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let ident = &input.ident;
     let ident_builder = format_ident!("{}Builder", ident);
 
-    let (req_field_names, req_field_types, opt_field_names, opt_field_types) =
-        get_field_names_and_types(&input);
+    let named_fields = match &input.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(named_fields),
+            ..
+        }) => &named_fields.named,
+        _ => panic!("expected a struct with named fields"),
+    };
 
-    let opt_field_stripped_types = opt_field_types.iter().map(|ty| get_stripped_type(&ty));
+    let builder_fields: Vec<_> = named_fields.iter().map(|f| get_builder_field(f)).collect();
 
-    let builder_struct = quote!(
-        pub struct #ident_builder {
-            #( #req_field_names: Option<#req_field_types> , )*
-            #( #opt_field_names: #opt_field_types , )*
-        }
-    );
-
-    let builder_setters = quote!(
-        #(
-        fn #req_field_names(&mut self, #req_field_names: #req_field_types) -> &mut Self {
-            self.#req_field_names = Some(#req_field_names);
-            self
-        }
-        )*
-
-        // optional types, uses stripped (Optional<> removed) type for input arg type
-        #(
-        fn #opt_field_names(&mut self, #opt_field_names: #opt_field_stripped_types) -> &mut Self {
-            self.#opt_field_names = Some(#opt_field_names);
-            self
-        }
-        )*
-    );
-
-    let builder_build_func = quote!(
-        pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
-            // None checks
-            #(
-            if self.#req_field_names.clone() == None {
-                return Err(concat!(stringify!(#req_field_names), " is None !!").into());
-            }
-            )*
-
-            // Return Ok() struct
-            Ok( #ident {
-                #( #req_field_names: self.#req_field_names.clone().unwrap(), )*
-                #( #opt_field_names: self.#opt_field_names.clone(), )*
-            } )
-        }
-    );
+    let builder_struct = builder_struct(&ident_builder, &builder_fields);
+    let builder_constructor = builder_constructor(&ident, &ident_builder, &builder_fields);
+    let builder_setters: Vec<_> = builder_fields.iter().map(|f| builder_setter(f)).collect();
+    let builder_build_func = builder_build_func(&ident, &builder_fields);
 
     let builder_funcs = quote!(
         impl #ident_builder {
-            #builder_setters
+            #(#builder_setters)*
             #builder_build_func
-        }
-    );
-
-    let builder_constructor = quote!(
-        impl #ident {
-            pub fn builder() -> #ident_builder {
-                #ident_builder {
-                    #( #req_field_names: None , )*
-                    #( #opt_field_names: None , )*
-                }
-            }
         }
     );
 
@@ -84,55 +42,137 @@ pub fn derive(input: TokenStream) -> TokenStream {
     })
 }
 
-fn get_field_names_and_types<'a>(
-    input: &'a DeriveInput,
-) -> (
-    Vec<&'a Option<syn::Ident>>,
-    Vec<&'a syn::Type>,
-    Vec<&'a Option<syn::Ident>>,
-    Vec<&'a syn::Type>,
-) {
-    let named_fields = match &input.data {
-        Data::Struct(DataStruct {
-            fields: Fields::Named(named_fields),
-            ..
-        }) => &named_fields.named,
-        _ => panic!("expected a struct with named fields"),
-    };
+// The fields that the builder operates on, with relevant type data
+struct BuilderField {
+    ident: syn::Ident,
+    typ: syn::Type,
+    is_opt: bool
+}
 
-    let is_optional = |named_field: &syn::Field| {
-        if let syn::Type::Path(path) = &named_field.ty {
-            path.path.segments[0].ident == syn::Ident::new("Option", Span::call_site())
-        } else {
-            false
+// Given an Option<T> type strip the option and return T
+fn strip_opt(path: &syn::TypePath) -> syn::Type {
+    if let PathArguments::AngleBracketed(args) = &path.path.segments[0].arguments {
+        if let GenericArgument::Type(Type::Path(p)) = &args.args[0] {
+            return syn::Type::Path(p.clone());
         }
-    };
+    }
+    panic!(concat!("Can't extract T from Option<T> for type", stringify!(path)));
+}
 
-    let (opt_fields, req_fields): (_, Vec<_>) =
-        named_fields.iter().partition(|field| is_optional(field));
+// get the type of a field. If the field is an Option<> then strip the option and set is_opt to true
+fn get_type(named_field: &syn::Field) -> (syn::Type, bool) {
 
-    let opt_field_names = opt_fields.iter().map(|field| &field.ident).collect();
-    let opt_field_types = opt_fields.iter().map(|field| &field.ty).collect();
+    if let syn::Type::Path(path) = &named_field.ty {
+        let is_opt = path.path.segments[0].ident == syn::Ident::new("Option", Span::call_site());
+        let typ = if is_opt {
+            strip_opt(path)
+        } else {
+            syn::Type::Path(path.clone())
+        };
+        (typ, is_opt)
+    } else {
+        panic!("can't get type")
+    }
+}
 
-    let req_field_names = req_fields.iter().map(|field| &field.ident).collect();
-    let req_field_types = req_fields.iter().map(|field| &field.ty).collect();
-
-    (
-        req_field_names,
-        req_field_types,
-        opt_field_names,
-        opt_field_types,
+// create the constructor for the builder (all fields = None)
+fn builder_constructor(ident: &syn::Ident, ident_builder: &syn::Ident, builder_fields: &Vec<BuilderField>) -> proc_macro2::TokenStream {
+    let field_names: Vec<_> = builder_fields.iter().map(|f| &f.ident).collect();
+    quote!(
+        impl #ident {
+            pub fn builder() -> #ident_builder {
+                #ident_builder {
+                    #( #field_names: None , )*
+                }
+            }
+        }
     )
 }
 
-// Given a type Optional<T> extract the ident of T
-fn get_stripped_type(opt_field_type: &syn::Type) -> &proc_macro2::Ident {
-    if let Type::Path(path) = &opt_field_type {
-        if let PathArguments::AngleBracketed(args) = &path.path.segments[0].arguments {
-            if let GenericArgument::Type(Type::Path(p)) = &args.args[0] {
-                return &p.path.segments[0].ident;
-            }
+// setters for the builder
+fn builder_setter(builder_field: &BuilderField) -> proc_macro2::TokenStream {
+    let field_name = &builder_field.ident;
+    let field_typ = &builder_field.typ;
+
+    quote!(
+        fn #field_name(&mut self, #field_name: #field_typ) -> &mut Self {
+            self.#field_name = Some(#field_name);
+            self
         }
+    )
+}
+
+// the build() function to convert from the builder to the buildee
+fn builder_build_func(ident: &syn::Ident, builder_fields: &Vec<BuilderField>) -> proc_macro2::TokenStream {
+
+    let none_checks: Vec<_> = builder_fields.iter().map(|f| none_check(f)).collect();
+    let set_fields: Vec<_> = builder_fields.iter().map(|f| set_field(f)).collect();
+
+    quote!(
+        pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
+            #(#none_checks)*
+
+            Ok( #ident {
+                #(#set_fields ,)*
+            })
+        }
+    )
+}
+
+// Check if a field is None and early return error if it is
+// Fields that are optional in the buildee are not checked
+fn none_check(field: &BuilderField) -> proc_macro2::TokenStream {
+    let field_name = &field.ident;
+    if field.is_opt {
+        quote!()    
+    } else {
+        quote!(
+            if self.#field_name.clone() == None {
+                return Err(concat!(stringify!(#field_name), " is None !!").into());
+            }
+        )
     }
-    panic!(concat!("Can't extract T from Option<T>: ", stringify!(opt_field_type)));
+}
+
+// set a fields in the buildee with the value in the builder
+fn set_field(field: &BuilderField) -> proc_macro2::TokenStream {
+    let field_name = &field.ident;
+    if field.is_opt {
+        quote!(#field_name: self.#field_name.clone())
+    } else {
+        quote!(#field_name: self.#field_name.clone().unwrap())
+    }
+}
+
+// Convert from a syn::Field to locally used BuilderField
+fn get_builder_field(field: &syn::Field) -> BuilderField {
+
+    if let Some(ident) = &field.ident {
+        let(typ, is_opt) = get_type(field);
+
+        BuilderField {
+            ident: ident.clone(),
+            typ,
+            is_opt
+        }
+    } else {
+        panic!("Can't extract BuilderField details")
+    }
+}
+
+// Return tokens for the builder struct
+fn builder_struct(builder_name: &syn::Ident, named_fields: &Vec<BuilderField>) -> proc_macro2::TokenStream {
+
+    let struct_fields = named_fields.iter().map(
+        |field: &BuilderField| { 
+            let (field_name, field_type) = (&field.ident, &field.typ);
+            quote!(#field_name: Option<#field_type>)
+        }
+    );
+
+    quote!(
+        pub struct #builder_name {
+            #(#struct_fields , )*
+        }
+    )
 }
